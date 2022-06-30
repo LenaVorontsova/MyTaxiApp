1f1c6b5edc5b7e1a8d406ea56f473a1b86ec3458
Elena Vorontsova <lena.voroncova26@yandex.ru>
2022-06-30 22:01:41 +0400

    add add: add customer location detection using gps

--- MyTaxi/app/src/main/java/com/example/mytaxi/CustomersMapActivity.java
+++ MyTaxi/app/src/main/java/com/example/mytaxi/CustomersMapActivity.java
@@ -1,30 +1,73 @@
 package com.example.mytaxi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
-
-import com.yandex.mapkit.Animation;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.ObjectEvent;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.RotationType;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.image.ImageProvider;
 
-public class CustomersMapActivity extends AppCompatActivity {
+public class CustomersMapActivity extends AppCompatActivity implements UserLocationObjectListener {
 
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
     private MapView mapView;
+    private UserLocationLayer userLocationLayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         MapKitFactory.setApiKey("68f94b44-d62b-4e49-b77a-95e18f13d4bd");
-        MapKitFactory.initialize(this);
+        MapKitFactory.initialize(CustomersMapActivity.this);
         setContentView(R.layout.activity_customers_map);
-        mapView = (MapView)findViewById(R.id.mapview2);
+        super.onCreate(savedInstanceState);
+        initMap();
+    }
+
+    private void initMap() {
+        mapView = (MapView) findViewById(R.id.mapview2);
+
+        mapView.getMap().setRotateGesturesEnabled(false);
         mapView.getMap().move(
-                new CameraPosition(new Point(55.751575, 37.573856), 11.0f, 0.0f, 0.0f),
-                new Animation(Animation.Type.SMOOTH, 0),
-                null);
+                new CameraPosition(
+                        new Point(56.852765, 53.206187),
+                        14,
+                        0,
+                        0
+                )
+        );
+
+        requestLocationPermission();
+
+        MapKit mapKit = MapKitFactory.getInstance();
+        mapKit.resetLocationManagerToDefault();
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+
+        userLocationLayer.setObjectListener(this);
+    }
+
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                "android.permission.ACCESS_FINE_LOCATION")
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{"android.permission.ACCESS_FINE_LOCATION"},
+                    PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
     }
 
     @Override
@@ -39,5 +82,50 @@ public class CustomersMapActivity extends AppCompatActivity {
         super.onStart();
         MapKitFactory.getInstance().onStart();
         mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onObjectAdded(UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float) (mapView.getWidth() * 0.5), (float) (mapView.getHeight() * 0.5)),
+                new PointF((float) (mapView.getWidth() * 0.5), (float) (mapView.getHeight() * 0.83)));
+
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, R.drawable.user_arrow));
+
+        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+
+        pinIcon.setIcon(
+                "icon",
+                ImageProvider.fromResource(this, R.drawable.icon),
+                new IconStyle().setAnchor(new PointF(0f, 0f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(0f)
+                        .setScale(1f)
+        );
+
+        pinIcon.setIcon(
+                "pin",
+                ImageProvider.fromResource(this, R.drawable.search_result),
+                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(1f)
+                        .setScale(0.5f)
+        );
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+    }
+
+    @Override
+    public void onObjectRemoved(UserLocationView view) {
+    }
+
+    @Override
+    public void onObjectUpdated(UserLocationView view, ObjectEvent event) {
     }
 }
